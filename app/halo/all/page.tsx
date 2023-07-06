218381d869b5287c7008018e3dda9c0495099154
m19e <nexus.me7290@gmail.com>
2023-07-07 02:19:33 +0900

    feat: switch halo from halo-list

--- app/halo/all/page.tsx
+++ app/halo/all/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from "next/image"
 import { Suspense, useState, useRef } from "react"
 import type { RefObject } from "react"
 import {
@@ -12,7 +13,10 @@ import {
 } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 
-const useCanvas = (): [RefObject<HTMLCanvasElement>, { save: () => void }] => {
+const useCanvas = (): [
+  RefObject<HTMLCanvasElement>,
+  { save: (id: string) => void },
+] => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
   const downloadURI = (uri: string, name: string) => {
@@ -24,10 +28,11 @@ const useCanvas = (): [RefObject<HTMLCanvasElement>, { save: () => void }] => {
     document.body.removeChild(link)
   }
 
-  const save = () => {
+  const save = (id: string) => {
     const uri = canvasRef.current?.toDataURL()
     if (uri) {
-      const fileName = `halo-${Date.now().toString(16)}.png`
+      // const fileName = `halo-${Date.now().toString(16)}.png`
+      const fileName = `${id}-${Date.now().toString(16)}.png`
       downloadURI(uri, fileName)
     }
   }
@@ -35,50 +40,93 @@ const useCanvas = (): [RefObject<HTMLCanvasElement>, { save: () => void }] => {
   return [canvasRef, { save }]
 }
 
-const students = [
-  { id: "hina", name: "ヒナ" },
-  { id: "ako", name: "アコ" },
-  { id: "iori", name: "イオリ" },
-  { id: "chinatsu", name: "チナツ" },
-]
+const STUDENTS = {
+  hoshino: "ホシノ",
+  nonomi: "ノノミ",
+  shiroko: "シロコ",
+  serika: "セリカ",
+  ayane: "アヤネ",
+  hina: "ヒナ",
+  ako: "アコ",
+  iori: "イオリ",
+  chinatsu: "チナツ",
+  aru: "アル",
+  kayoko: "カヨコ",
+  haruka: "ハルカ",
+  mutsuki: "ムツキ",
+} as const
+
+type StudentID = keyof typeof STUDENTS
 
 export default function All() {
-  const [haloID, setHaloID] = useState("hoshino")
+  const [haloID, setHaloID] = useState<StudentID>("hoshino")
   const [canvasRef, { save }] = useCanvas()
 
-  const options = students.map(s => (
-    <option key={s.id} value={s.id}>
-      {s.name}
-    </option>
-  ))
-
   return (
-    <div className="min-h-screen">
-      <h1>#ブルアカ #ヘイロー #3D</h1>
-      <div className="flex flex-col items-center gap-4">
-        <select
-          className="select min-w-[8rem]"
-          onChange={e => setHaloID(e.currentTarget.value)}
-        >
-          <option value="hoshino">ホシノ</option>
-          <option value="nonomi">ノノミ</option>
-          <option value="shiroko">シロコ</option>
-          <option value="serika">セリカ</option>
-          <option value="ayane">アヤネ</option>
-          {options}
-        </select>
-        <div className="rounded-box h-80 w-80 bg-slate-800">
-          <Canvas
-            ref={canvasRef}
-            camera={{ position: [0, 0, 1] }}
-            gl={{ preserveDrawingBuffer: true }}
-          >
-            <Halo key={haloID} id={haloID} />
-          </Canvas>
+    <div className="drawer">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        {/* Page content here */}
+        <div className="bg-triangle min-h-screen">
+          <h1>#ブルアカ #ヘイロー #3D</h1>
+          <label htmlFor="my-drawer" className="btn-primary drawer-button btn">
+            Open drawer
+          </label>
+
+          <div className="flex flex-col items-center gap-4">
+            <div className="rounded-box h-80 w-80 bg-slate-800">
+              <Canvas
+                key={haloID}
+                ref={canvasRef}
+                camera={{ position: [0, 0, 1] }}
+                gl={{ preserveDrawingBuffer: true }}
+              >
+                <Halo id={haloID} />
+              </Canvas>
+            </div>
+            <button className="btn" onClick={() => save(haloID)}>
+              save
+            </button>
+
+            <div className="bg-white p-1">
+              <div className="grid grid-cols-6 gap-2 rounded bg-gray-200 p-2">
+                {Object.entries(STUDENTS).map(([id, name]) => (
+                  <div
+                    key={id}
+                    className="rounded-box flex cursor-pointer flex-col gap-2 bg-white p-1 shadow-md"
+                    onClick={() => setHaloID(id as StudentID)}
+                  >
+                    <Image
+                      className="rounded-box relative aspect-square h-16 w-16 bg-slate-800"
+                      src={`/thumbnail/${id}.png`}
+                      alt={`${id} thumbnail`}
+                      width={480}
+                      height={480}
+                      priority
+                    />
+                    <span className="bg-slate-600 text-center text-sm text-white">
+                      {name}
+                    </span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
-        <button className="btn" onClick={save}>
-          save
-        </button>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer" className="drawer-overlay"></label>
+        <ul className="menu h-full w-80 overflow-y-auto bg-base-200 p-4 text-base-content">
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
       </div>
     </div>
   )
